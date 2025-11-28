c3e59a84f6209a68cb4ef4db72f7dca656b07d3a
Justin <jmackie4@asu.edu>
2025-11-28 12:59:58 -0700

    Initial versions of main files for project

---
+++ main.py
@@ -0,0 +1,175 @@
+import pandas as pd
+import numpy as np
+import os,nltk,re,random
+from collections import defaultdict,Counter
+import Data_Processor as dp
+import util_datastructs as ud
+from typing import List
+
+
+class Language_Model:
+    def __init__(self,corpus:dp.DataProcessor,tokenizer:dp.Tokenizer):
+        self.corpus = corpus.get_corpus()
+        self.tokenizer = tokenizer
+        
+    def set_language(self):
+        assert isinstance(self.corpus,pd.DataFrame)
+        while True:
+            print(f'{self.corpus.iloc[0,0]} | {self.corpus.iloc[0,1]}')
+            user_choice = input('Please select the language with 0 or 1: ')
+            if int(user_choice) in [0,1]:
+                break
+            else:
+                print('Please give a valid answer!')
+        self.corpus = self.corpus.iloc[:,int(user_choice)]
+        print('New Corpus \n {}'.format(self.corpus))
+
+
+    def tokenize_corpus(self):
+        assert isinstance(self.corpus,pd.Series)
+        converted_corpus = self.corpus.tolist()
+        tokenized_corpus = [self.tokenizer.tokenize(x) for x in converted_corpus]
+        return tokenized_corpus
+
+    def organize_corpus(self):
+        pretrain_corpus:List[str] = []
+        tokenized_corpus = self.tokenize_corpus()
+        assert isinstance(tokenized_corpus,list)
+        tokenized_corpus = [x.split(' ') for x in tokenized_corpus]
+        for item in tokenized_corpus:
+            item.insert(0,'<START>')
+            item.append('<END>')
+            pretrain_corpus.extend(item)
+        return pd.Series(pretrain_corpus)
+
+
+class N_Gram_Model(Language_Model):
+    def __init__(self,corpus:dp.DataProcessor,tokenizer:dp.Tokenizer,n=2):
+        super().__init__(corpus,tokenizer)
+        self.n = n
+        self.set_language()
+        self.n_gram_table = None
+
+    def create_n_gram_table(self):
+        assert isinstance(self.corpus,pd.Series)
+        organized_corpus: pd.Series = self.organize_corpus()
+        n_gram_counts: defaultdict[str, Counter] = defaultdict(Counter)
+        n_gram_index: List[str] = list(organized_corpus.unique())
+        i = 0
+        while True:
+            try:
+                organized_corpus.iloc[i+self.n]
+            except IndexError:
+                break
+            else:
+                if self.n == 1:
+                    n_gram_counts[organized_corpus.iloc[i]] += 1
+                    i += 1
+                elif self.n > 1:
+                    n_gram_slice = organized_corpus.iloc[i:i+self.n].tolist()
+                    assert len(n_gram_slice) == self.n
+                    n_gram_counts[' '.join(n_gram_slice[:-1])][n_gram_slice[-1]] += 1
+                    i += 1
+
+        if self.n == 1:
+            final_output: pd.Series = pd.Series(n_gram_counts,index=n_gram_index)
+            final_output = final_output.div(final_output.sum())
+            self.n_gram_table = final_output
+            print(self.n_gram_table)
+        else:
+            final_output: pd.DataFrame = pd.DataFrame.from_dict(n_gram_counts,orient='index',columns=n_gram_index)
+            filled_output = final_output.fillna(0)
+            filled_output = filled_output + 1
+            self.n_gram_table = filled_output.div(filled_output.sum(axis=1),axis=0)
+            print(self.n_gram_table)
+
+
+    def predict(self):
+        assert self.n_gram_table is not None
+        sent = []
+
+
+    def create_and_predict(self):
+        self.create_n_gram_table()
+        self.predict()
+
+class Hub():
+    def __init__(self):
+        self.processor = dp.DataProcessor()
+        self.tokenizer = dp.Tokenizer()
+        self.corpus = None
+
+    def get_processor(self):
+        return self.processor
+
+    def get_tokenizer(self):
+        return self.tokenizer
+
+    def get_text(self):
+        self.processor.get_text()
+
+    def get_titles(self):
+        self.processor.get_titles()
+
+    def get_corpus(self):
+        assert self.corpus is not None
+        return self.corpus
+
+    def set_corpus(self):
+        self.corpus = self.processor.get_corpus()
+
+    def find_token_sequence(self):
+        if self.corpus is None:
+            self.set_corpus()
+        while True:
+            print(f'{self.corpus.iloc[0, 0]} | {self.corpus.iloc[0, 1]}')
+            user_choice = input('Please select the language with 0 or 1: ')
+            if int(user_choice) in [0, 1]:
+                break
+            else:
+                print('Please give a valid answer!')
+
+        sents = self.corpus.iloc[:, int(user_choice)]
+        sents.apply(lambda x: ' '.join(self.tokenizer.tokenize(x)))
+        user_sequence = input('Please enter your sequence: ')
+        filter = sents.str.contains(user_sequence.lower(), case=False)
+        print(self.corpus[filter])
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main_hub = Hub()
+    model = N_Gram_Model(main_hub.get_processor(),main_hub.get_tokenizer())
+    options = {'get text': main_hub.get_text,'get titles': main_hub.get_titles,'n_grams':model.create_and_predict,
+               'find sequence': main_hub.find_token_sequence}
+    while True:
+        for i,item in enumerate(options):
+            print(f'{i}: {item}',end='\n')
+        users_choice = input('Please enter your choice: ')
+        if users_choice.lower() in options :
+            options[users_choice.lower()]()
+        elif users_choice.lower() == 'exit':
+            break
+        else:
+            print('Please enter a valid choice!')
+
+
+
+
+
+
+
+
+
+
+        
+        
+
+    
+        
+        
---
+++ util_datastructs.py
@@ -0,0 +1,91 @@
+import pandas as pd
+import numpy as np
+import nltk,os,re
+from collections import defaultdict,Counter
+
+class Node():
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+    def get_data(self):
+        return self.data
+
+    def get_next(self):
+        return self.next
+
+    def set_data(self,data):
+        self.data = data
+
+    def set_next(self,next):
+        self.next = next
+
+
+class LinkedList():
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head == None
+
+    def add_node(self,data):
+        new_node = Node(data)
+        new_node.set_next(self.head)
+        self.head = new_node
+
+    def get_data(self,data):
+        current = self.head
+        found = False
+        result = None
+        while current != None and not found:
+            if current.get_data() == data:
+                result = current.get_data()
+                found = True
+            else:
+                current = current.get_next()
+        return result
+
+
+class Stack():
+    def __init__(self):
+        self.items = []
+
+    def push(self,data):
+        self.items.append(data)
+
+    def is_empty(self):
+        return self.items == []
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+    def clear(self):
+        self.items = []
+
+class Queue():
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def push(self,data):
+        self.items.append(data)
+
+    def pop(self):
+        return self.items.pop(0)
+
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+    def return_queue(self):
+        return self.items

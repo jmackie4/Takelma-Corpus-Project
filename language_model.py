f55b9b33fcd21fcb63b719b35abf3c5a5460e0c3
Justin <jmackie4@asu.edu>
2025-12-13 12:57:04 -0700

    Newest version of language_model just need to modify generate sentence function;

--- language_model.py
+++ language_model.py
@@ -5,73 +5,42 @@ from collections import defaultdict, Counter
 import Data_Processor as dp
 import util_datastructs as ud
 from typing import List
+from nltk.util import ngrams,everygrams
+from nltk.lm import MLE,StupidBackoff
+from nltk.lm.preprocessing import pad_sequence,pad_both_ends,flatten,padded_everygram_pipeline
 
-def organize_corpus(corpus:pd.DataFrame,tokenizer:dp.Tokenizer):
+def create_model(corpus:pd.DataFrame,tokenizer:dp.Tokenizer,n:int=2):
     while True:
-        user_choice = input(f'{corpus.iloc[0,0]} | {corpus.iloc[0,1]}' + '\n' + 'Enter a choice: ')
+        print(f'{corpus.iloc[0, 0]} | {corpus.iloc[0, 1]}')
+        user_choice = input('Please choose one of the following options: ')
         try:
-            int(user_choice) in [0,1] == True
+            int(user_choice) in [0,1]
         except ValueError:
-            print('Please enter a valid integer')
+            print('Please pick a valid option!')
         else:
             break
+    user_language = corpus.iloc[:,int(user_choice)]
+    tokenized_corpus = user_language.apply(lambda x: tokenizer.tokenize(x).split(' '))
+    n_grams,vocabulary = padded_everygram_pipeline(n,tokenized_corpus.tolist())
+    model = StupidBackoff(order=n)
+    model.fit(n_grams,vocabulary)
+    return model
 
-    sentences = corpus.iloc[:,int(user_choice)].apply(lambda x: tokenizer.tokenize(x))
-
-    formatted_corpus = []
-    for sentence in sentences.tolist():
-        tokens:List[str] = sentence.split(' ')
-        tokens.insert(0,'<START>')
-        tokens.append('<END>')
-        formatted_corpus.extend(tokens)
-
-    return formatted_corpus
-
-
-def ngram_decorator(func):
-    def wrapper(corpus:pd.DataFrame,tokenizer:dp.Tokenizer,n:int=2):
-        models = {}
-        formatted_corpus = organize_corpus(corpus,tokenizer)
-        while n >= 1:
-            models[f'{n}-gram model'] = func(formatted_corpus,n)
-            print(f'{n}-gram model')
-            print(models[f'{n}-gram model'])
-            n -= 1
-        return models
-    return wrapper
-
-@ngram_decorator
-def create_ngrams(corpus:List[str],n):
-    if n == 1:
-        unigram_table = pd.Series(Counter(corpus))
-        unigram_table.drop(['<START>','<END>'],inplace=True)
-        return unigram_table.div(unigram_table.sum())
-
+def generate_sentence(model:nltk.lm.api.LanguageModel,starting_seed=None,n:int=10) -> List[str]:
+    assert model.vocab and model.counts, 'You need a trained model to generate a sentence!'
+    if starting_seed:
+        output = model.generate(num_words=n,text_seed=[starting_seed])
     else:
-        n_grams = defaultdict(lambda: defaultdict(int))
-        i = 0
-        while i + n <= len(corpus):
-            ngram_window = corpus[i:i+n]
-            n_grams[' '.join(ngram_window[:-1])][ngram_window[-1]] += 1
-            i += 1
+        output = model.generate(num_words=n,text_seed=['<s>'])
 
-        ngram_table = pd.DataFrame.from_dict(n_grams)
-        ngram_table = ngram_table.fillna(0)
-        ngram_table = ngram_table + 1
-        return ngram_table.div(ngram_table.sum(axis=0),axis=1)
+    print(output)
+    return output
 
 
-def generate(model,min_tokens:int = 3,max_tokens:int = 10) -> str:
-    if isinstance(model,pd.Series):
-        num_tokens = random.randint(min_tokens,max_tokens)
-        sentence_stack = []
-        for _ in range(num_tokens):
-            sentence_stack.append(random.choices(model.index,weights=model.values,k=1)[0])
-        return ' '.join(sentence_stack)
 
-    else:
-        sentence = ud.LinkedList()
-        sentence.add_node('<START>')
+
+
+
 
 
 
@@ -80,9 +49,10 @@ def generate(model,min_tokens:int = 3,max_tokens:int = 10) -> str:
 
 
 processor = dp.DataProcessor()
-corpus = processor.get_corpus()
 tokenizer = dp.Tokenizer()
-create_ngrams(corpus,tokenizer,3)
+corpus = processor.get_corpus()
+model = create_model(corpus,tokenizer,n=2)
+generate_sentence(model,n=15)
 
 #/Users/justinmackie/Dropbox/Mac/Desktop/Coding Projects/Takelma Corpus Project
 #Parallel Texts

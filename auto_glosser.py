26960f457d09ff097f358e908d2377f5174fbbdf
Justin <jmackie4@asu.edu>
2025-12-17 13:30:05 -0700

    Added the glosser base class and the tfidf glosser class, I still need to add an entropy based glosser

--- auto_glosser.py
+++ auto_glosser.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import nltk, spacy
 import Data_Processor as dp
-from typing import Tuple
+from typing import Tuple,List
 from nltk.lm.preprocessing import flatten
 from nltk.lm.vocabulary import Vocabulary
 
@@ -16,9 +16,6 @@ def get_vocabularies(corpus:pd.DataFrame,tokenizer:dp.Tokenizer) -> Tuple[nltk.l
                                                         token.tag_ not in tag_filter and not token.is_punct])
     source_vocab = Vocabulary(flatten(source_language.tolist()))
     target_vocab = Vocabulary(flatten(target_language.tolist()))
-
-    print('Source vocabulary: ',source_vocab.counts.keys())
-    print('Target vocabulary: ',target_vocab.counts.keys())
     return (source_vocab,target_vocab)
 
 
@@ -42,14 +39,12 @@ def fill_gloss_table(corpus:pd.DataFrame,gloss_table:pd.DataFrame,tokenizer:dp.T
 
         gloss_table.loc[list(source_sent),list(target_sent)] = gloss_table.loc[list(source_sent),list(target_sent)] + 1
 
-    print(gloss_table.head())
     return gloss_table
 
 def create_tf_table(gloss_table:pd.DataFrame) -> pd.DataFrame:
     gloss_table_copy = gloss_table.copy()
     gloss_table_copy[gloss_table_copy > 0] = np.log10(gloss_table_copy[gloss_table_copy > 0])
     gloss_table_copy[gloss_table_copy > 0] = gloss_table_copy[gloss_table_copy > 0] + 1
-    print(gloss_table_copy.head())
     return gloss_table_copy
 
 def create_idf_vector(gloss_table:pd.DataFrame) -> pd.Series:
@@ -58,15 +53,61 @@ def create_idf_vector(gloss_table:pd.DataFrame) -> pd.Series:
     idf_numerator_list = [len(bool_table.index)] * len(bool_table.columns)
     idf_numerator_vector = pd.Series(idf_numerator_list, index=idf_denominator_vector.index)
     idf_vector = np.log10(idf_numerator_vector.div(idf_denominator_vector))
-    print(pd.Series(idf_vector))
     return pd.Series(idf_vector)
 
 def create_tfidf_table(gloss_table:pd.DataFrame) -> pd.DataFrame:
     tf_table = create_tf_table(gloss_table)
     idf_vector = create_idf_vector(gloss_table).to_numpy()
     tfidf_table = tf_table.mul(idf_vector)
-    print(tfidf_table.head())
     return tfidf_table
+
+
+class Glosser: #Base class for glosser
+    def __init__(self,corpus:pd.DataFrame,tokenizer:dp.Tokenizer) -> None:
+        self.corpus = corpus
+        self.tokenizer = tokenizer
+        self.vocabularies = None
+        self.gloss_table = None
+        self.set_vocabularies()
+        self.set_gloss_table()
+
+    def set_vocabularies(self):
+        self.vocabularies = get_vocabularies(self.corpus,self.tokenizer)
+
+    def set_gloss_table(self):
+        assert self.vocabularies is not None, 'You must create vocabularies first!'
+        gloss_table = create_gloss_table(self.vocabularies)
+        self.gloss_table = fill_gloss_table(self.corpus,gloss_table,self.tokenizer)
+
+    def align_sentence(self,sentence:pd.Series) -> List[Tuple[str,str]]:
+        raise NotImplementedError
+
+
+class Tfidf_Glosser(Glosser):
+    def __init__(self,corpus:pd.DataFrame,tokenizer:dp.Tokenizer) -> None:
+        super().__init__(corpus,tokenizer)
+        self.tfidf_table = None
+        self.set_tfidf_table()
+
+    def set_tfidf_table(self):
+        assert self.vocabularies is not None, 'You must create vocabularies first!'
+        assert self.gloss_table is not None, 'You must create a gloss table first!'
+        self.tfidf_table = create_tfidf_table(self.gloss_table)
+
+    def align_sentence(self,sentence:pd.Series) -> List[Tuple[str,str]]:
+        nlp = spacy.load('en_core_web_sm')
+        tag_filter = {'PRP', 'PRP$', 'PUNCT', 'ADP', 'DT'}
+        tokenized_source = self.tokenizer.tokenize(sentence.iloc[0]).split(' ')
+        tokenized_target = [token.lemma_.lower() for token in nlp(sentence.iloc[1]) if token.tag_ not in tag_filter
+                            and not token.is_punct]
+        tfidf_slice = self.tfidf_table.loc[tokenized_source,tokenized_target]
+        print(tfidf_slice.idxmax(axis=1))
+
+
+
+
+
+
 
 
 

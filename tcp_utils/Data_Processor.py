f509221cb0f4ab6e782a8ccb4ded5d050cfb319a
Justin M <jmackie4@asu.edu>
2026-01-07 19:46:33 -0700

    Finished the new pipeline that converts a file path to a corpus dataframe, I still just need to either edit hub so it can just use the output from the create corpus method, the tokenizer is also finished too, that will be treated like the inital pipeline object for the different models and parts of the project that need to use tokenized texts

--- tcp_utils/Data_Processor.py
+++ tcp_utils/Data_Processor.py
@@ -3,32 +3,10 @@ import nltk,os,re
 from sklearn.base import BaseEstimator, TransformerMixin
 import spacy
 from typing import Dict
-
-class Tokenizer_Transformer(BaseEstimator, TransformerMixin):
-    def __init__(self, pattern: str = None):
-        self._pattern = pattern
-        self._nlp = spacy.load('en_core_web_sm')
-        self._tag_filter = {'PRP', 'PRP$', 'PUNCT', 'ADP', 'DT'}
-
-    def fit(self, X, y=None):
-        if self.pattern is not None:
-            self.tokenizer_ = nltk.RegexpTokenizer(self.pattern)
-        else:
-            pass
-
-        return self
-
-    def transform(self, X: pd.DataFrame):
-        X.iloc[:,0] = X.iloc[:, 0].apply(lambda x: ' '.join(self.tokenizer_.tokenize(x.lower())))
-        X.iloc[:,1] = X.iloc[:, 1].apply(lambda x: [token.lemma_.lower() for token in self._nlp(x)
-                                                         if token.tag_ not in self._tag_filter and not
-                                                         token.is_punct])
-        return X
-
+from sklearn.pipeline import Pipeline
 
 #The following just takes a root folder that holds the two directories for the parallel texts
 #and it returns a dictionary that has the source language dir path and target language dir path
-#I still need to add a wrapper that allows you to feed the root path into the transformer
 class Corpus_Transformer(BaseEstimator, TransformerMixin):
     def __init__(self):
         pass
@@ -55,6 +33,7 @@ class Corpus_Transformer(BaseEstimator, TransformerMixin):
         return output_dict
 
 
+
 class Corpus_Loader(BaseEstimator, TransformerMixin):
     def __init__(self):
         pass
@@ -64,8 +43,63 @@ class Corpus_Loader(BaseEstimator, TransformerMixin):
     def transform(self,X:Dict[str,str]):
         ''' This corpus loader transformer needs to be with the Corpus transformer object in a
         pipeline in order to work!'''
+        i = 0
+        source_sents = []
+        target_sents = []
+        text_idxs = {}
         for file in os.listdir(X['source_language']):
             with open(os.path.join(X['source_language'],file),'r',encoding='utf-8') as f:
+                  source_sents.extend([line[:-1] for line in f.readlines()])
+                  text_idxs[file] = i,len(source_sents)
+                  i = len(source_sents)
+
+            with open(os.path.join(X['target_language'],file),'r',encoding='utf-8') as f:
+                  target_sents.extend([line.rstrip('\n') for line in f.readlines()])
+
+        output_dict = {'text_idxs':text_idxs,'source_sents':source_sents,'target_sents':target_sents}
+        return output_dict
+
+
+
+def create_corpus():
+    while True:
+        user_path = input('Please give the path of the root directory! ')
+        if os.path.isdir(user_path):
+            break
+        else:
+            print('The provided path is not a directory!')
+
+    pipeline = Pipeline([('corpus_transformer', Corpus_Transformer()),
+                         ('corpus_loader', Corpus_Loader())])
+    pipeline_output = pipeline.fit_transform(user_path)
+    corpus_dataframe = pd.DataFrame({'source_language': pipeline_output['source_sents'],
+                                     'target_language': pipeline_output['target_sents'],
+                                     })
+    return corpus_dataframe
+
+
+
+class Tokenizer_Transformer(BaseEstimator, TransformerMixin):
+    def __init__(self, pattern: str = None):
+        self._pattern = pattern
+        self._nlp = spacy.load('en_core_web_sm')
+        self._tag_filter = {'PRP', 'PRP$', 'PUNCT', 'ADP', 'DT'}
+
+    def fit(self, X, y=None):
+        if self._pattern is not None:
+            self.tokenizer_ = nltk.RegexpTokenizer(self._pattern)
+        else:
+            pass
+
+        return self
+
+    def transform(self, X: pd.DataFrame):
+        X.iloc[:,0] = X.iloc[:, 0].apply(lambda x: ' '.join(self.tokenizer_.tokenize(x.lower())))
+        X.iloc[:,1] = X.iloc[:, 1].apply(lambda x: ' '.join([token.lemma_.lower() for token in self._nlp(x)
+                                                         if token.tag_ not in self._tag_filter and not
+                                                         token.is_punct]))
+        return X
+
 
 
 
